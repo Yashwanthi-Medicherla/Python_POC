121c8c234ca2c6b9d2d9259dcd6f7783c47e5c94
Yashwanthi-Medicherla <81857914+Yashwanthi-Medicherla@users.noreply.github.com>
2021-04-05 18:48:32 +0530

    Add files via upload
    Reading an Excel and Creating a Dataframe

---
+++ Reading_Excel.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+#reading the excel file into a data frame
+
+dataframe = pd.read_excel("C:/Users/medicherla.y/Desktop/Student_DB.xlsx")
+
+#Adding a column to data frame
+
+
+
+print(dataframe)
